28280e62b253499390fd6c20fa1a188ef16c9804
Jemuel Bryan Vergara <jem@liquidloans.io>
2023-09-09 21:39:39 +0800

    LL-1271: Fixed podinit.sh and req.txt

--- podinit.sh
+++ podinit.sh
@@ -8,6 +8,19 @@ cd ../telliot-core
 python3 change_address.py
 cat ./src/telliot_core/data/contract_directory.json
 
+#Set Volume Mounts as Environment Variable
+token_file_path="/mnt/twilio-auth-token/dvm-${ENV_NAME}-twilio-auth-token"
+sid_file_path="/mnt/twilio-account-sid/dvm-${ENV_NAME}-twilio-account-sid"
+pk_file_path="/mnt/private-key/dvm-${ENV_NAME}-private-key"
+
+token_file_contents=$(cat "$token_file_path")
+sid_file_contents=$(cat "$sid_file_path")
+pk_file_contents=$(cat "$pk_file_path")
+
+export TWILIO_AUTH_TOKEN="$token_file_contents"
+export TWILIO_ACCOUNT_SID="$sid_file_contents"
+export PK="$pk_file_contents"
+
 #ADD CHAINED
 expect_script=$(expect -c "
 spawn chained add dvm \"$PK\" \"$NETWORK_ID\"
