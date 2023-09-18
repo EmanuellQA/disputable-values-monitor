ad7ddd1d41993698acb39344ecf46f2604712ef8
Jenkins-ID <patrick.endangan@gmail.com>
2023-09-18 16:17:33 +0700

    runnning as a service

---
+++ runtime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source .env
+source vars.example.sh
+#
+expect_script=$(expect -c "
+spawn chained add dvm \"$PK\" \"$NETWORK_ID\"
+log_file expect_log.txt
+expect \"Enter encryption password for dvm:\"
+send \"\r\"
+expect \"Confirm password:\"
+send \"\r\"
+expect eof
+")
+echo "$expect_script" | expect
+sleep 2
+pip install pexpect
+python expect.py
