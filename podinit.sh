56bb9360a21970f734fdde40cc4801bd8312708f
Jemuel Bryan Vergara <jem@liquidloans.io>
2023-09-20 18:56:52 +0800

    LL-1456: Adjusted podinit script

--- podinit.sh
+++ podinit.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
-pip install .
+#!/bin/bash
 
-python -c "import telliot_core; print(f'telliot-core version installed - {telliot_core.__version__}')"
-python -c "import telliot_feeds; print(f'telliot-feeds version installed - {telliot_feeds.__version__}')"
-
-cd ../telliot-core
 python3 change_address.py
-cat ./src/telliot_core/data/contract_directory.json
+cat "/usr/local/lib/python3.10/site-packages/telliot_core/data/contract_directory.${ENV_NAME}.json"
 
 #Set Volume Mounts as Environment Variable
 token_file_path="/mnt/twilio-auth-token/dvm-${ENV_NAME}-twilio-auth-token"
@@ -21,7 +16,6 @@ export TWILIO_AUTH_TOKEN="$token_file_contents"
 export TWILIO_ACCOUNT_SID="$sid_file_contents"
 export PK="$pk_file_contents"
 
-#ADD CHAINED
 expect_script=$(expect -c "
 spawn chained add dvm \"$PK\" \"$NETWORK_ID\"
 log_file expect_log.txt
@@ -31,18 +25,7 @@ expect \"Confirm password:\"
 send \"\r\"
 expect eof
 ")
-
-#RUN DVM
-cd ../disputable-values-monitor
-while true; do
-    expect_script=$(expect -c '
-        spawn cli -a dvm -d
-        expect "Enter password for dvm account:"
-        send "\r"
-        expect eof
-    ')
-
-    echo "$expect_script"
-
-    sleep 2
-done
+echo "$expect_script" | expect
+sleep 2
+pip install pexpect
+python expect.py
